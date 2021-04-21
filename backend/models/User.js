322ebd60a163448c4e980228cd40067e87ebac08
jmscottimd <jmscottimd@protonmail.com>
2021-04-21 18:03:33 -0400

    create user model

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    isAdmin:{
+        type: boolean,
+        required: false,
+    },
+},{timestamps: true});
+
+userSchema.methods.matchPassword = async function  (enterPassword) {
+    return await bcrypt.compare(enterPassword, this.password);
+
+};
+userSchema.pre('save', async function  (next) {
+    if (!this.isModified('password')){
+        next()
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = bcrypt.hash(this.password, salt);
+
+})
+module.exports = User = mongoose.model('User', userSchema)
